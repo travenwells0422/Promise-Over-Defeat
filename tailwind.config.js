efab10d78314c824c2c494195d81352ea08fdd5f
travenwells0422 <travendwellsSoftwareDeveloper@outlook.com>
2023-02-04 03:24:28 -0500

    layout under construction

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./components/**/*.{html,js}",
+        "./src/**/*.{html,js}",
+        "./index.html",
+        "./profile.html",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
